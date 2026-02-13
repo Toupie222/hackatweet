df82f13a104ead927bdc6198373ed0f4edc0fe8b
Timothee <dray.timothee@gmail.com>
2026-02-13 09:21:12 +0100

    Trends

--- frontend/components/Trends.js
+++ frontend/components/Trends.js
@@ -1,7 +1,23 @@
+import styles from "../styles/Trends.module.css"
+
 
 function Trends() {
     return(
-        <div></div>
+        <div className={styles.body}>
+            <h2 className={styles.titre}>
+                Trends
+            </h2>
+            <div className={styles.container}>
+                <div className={styles.trends}>
+                    <p className={styles.trendName}>#hackatweet</p>
+                    <p className={styles.nbTrend}>2 Tweets</p>
+                </div>
+                <div className={styles.trends}>
+                    <p className={styles.trendName}>#first</p>
+                    <p className={styles.nbTrend}>1 Tweet</p>
+                </div>
+            </div>
+        </div>
     );
 }
 
