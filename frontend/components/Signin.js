a08ccb24e5f80871754ab8bf358c9e042a61661a
Nicolas <nicolas.dumont.important@gmail.com>
2026-02-13 10:25:38 +0100

    Route incluse signup signin

--- frontend/components/Signin.js
+++ frontend/components/Signin.js
@@ -1,7 +1,33 @@
 import Popup from 'reactjs-popup';
 import styles from "../styles/Signin.module.css";
+import { useState } from 'react';
+import { useRouter } from 'next/navigation'
 
 function Signin() {
+
+    const router = useRouter()
+    const [signInUsername, setSignInUsername] = useState('');
+	const [signInPassword, setSignInPassword] = useState('');
+
+    function handleClick()
+    {
+        fetch(`http://localhost:3000/users/signin`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ username: signInUsername, password: signInPassword }),
+        }).then(response => response.json())
+            .then(data => {
+                if (data.result) {
+                    /*
+                    dispatch(login({username:signInUsername, token:data.token}));
+                    setSignInUsername('');
+                    setSignInPassword('');
+                    */
+                   router.push('/acceuil')
+                }
+            });
+    }
+
     return (
         <Popup
             trigger={<button className={styles.signin}>Sign in</button>}
@@ -22,9 +48,9 @@ function Signin() {
                             <br />
                         <img src="twitter.png" alt="Logo" className={styles.logo}/>
                         <h4 className={styles.text_popup}>Connect to Hackatweet</h4>
-                        <input className={styles.input} placeholder = "Username"/>
-                        <input className={styles.input} type="password" placeholder = "Password"/>
-                        <button className={styles.signin_popup}>Sign in</button>
+                        <input className={styles.input} placeholder = "Username" onChange={(e) => setSignInUsername(e.target.value)} value={signInUsername}/>
+                        <input className={styles.input} type="password" placeholder = "Password" onChange={(e) => setSignInPassword(e.target.value)} value={signInPassword}/>
+                        <button className={styles.signin_popup} onClick={() => handleClick()}>Sign in</button>
                     </div>
                 </div>
             )}
--- frontend/components/Signup.js
+++ frontend/components/Signup.js
@@ -1,8 +1,34 @@
 import Popup from 'reactjs-popup';
 import styles from "../styles/Signup.module.css";
-
+import { useState } from 'react';
+import { useRouter } from 'next/navigation'
 
 function Signup() {
+
+    const router = useRouter()
+    const [signUpFirstName, setSignUpFirstName] = useState('');
+	const [signUpUsername, setSignUpUsername] = useState('');
+	const [signUpPassword, setSignUpPassword] = useState('');
+
+    function handleClick()
+    {
+        fetch(`http://localhost:3000/users/signup`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ firstname:signUpFirstName, username: signUpUsername, password: signUpPassword }),
+        }).then(response => response.json())
+            .then(data => {
+                if (data.result) {
+                    /*
+                    dispatch(login({username:signUpUsername, token:data.token}));
+                    setSignInUsername('');
+                    setSignInPassword('');
+                    */
+                   router.push('/acceuil')
+                }
+            });
+    }
+    
     return (
         <Popup
             trigger={<button className={styles.signup}>Sign up</button>}
@@ -23,10 +49,10 @@ function Signup() {
                             <br />
                         <img src="twitter.png" alt="Logo" className={styles.logo}/>
                         <h4 className={styles.text_popup}>Create your Hackatweet account</h4>
-                        <input className={styles.input} placeholder = "Firstname"/>
-                        <input className={styles.input} placeholder = "Username"/>
-                        <input className={styles.input} type="password" placeholder = "Password"/>
-                        <button className={styles.signup_popup}>Sign up</button>
+                        <input className={styles.input} placeholder = "Firstname" onChange={(e) => setSignUpFirstName(e.target.value)} value={signUpFirstName}/>
+                        <input className={styles.input} placeholder = "Username" onChange={(e) => setSignUpUsername(e.target.value)} value={signUpUsername}/>
+                        <input className={styles.input} type="password" placeholder = "Password" onChange={(e) => setSignUpPassword(e.target.value)} value={signUpPassword} />
+                        <button className={styles.signup_popup} onClick={() => handleClick()}>Sign up</button>
                     </div>
                 </div>
             )}
